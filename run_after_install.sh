a3db22311c196fe4e90cf2d9324e203bfec08554
kyych <m.biernat43@gmail.com>
2022-05-16 14:33:40 +0200

    download dwm and install

--- run_after_install.sh
+++ run_after_install.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+# RUN after installing ARCH with profile XORG
+
+function download_basic_tools(){
+    sudo pacman -S git python make gcc wget libX11-devel libXft-devel libXinerama-devel
+}
+
+function download_and_install_DWM(){
+    git clone git://git.suckless.org/dwm;
+    cd dwm;
+    make clean;
+    make all clean;
+    sudo make clean install;
+    touch ~/.xinitrc
+    echo "dwm &" >> ~/.xinitrc
+    cd ~;
+}
+
+download_basic_tools
+download_and_install_DWM
